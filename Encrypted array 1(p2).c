8d0350b56d6d0e24126e2a8cc0c3daa0bf0f0300
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2021-11-23 11:41:55 +0530

    Create Encrypted array 1(p2).c

---
+++ Encrypted array 1(p2).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    
+    int i,j, p;
+    for( i=0;i<n;i++)
+    {
+        p=0;
+        for( j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+                p=p+a[j];
+            }
+
+        }
+        printf("%d ",p);
+    }
+    return 0;
+}
